3573f0ce2461f564b17a2418a5bc12a04acc8baf
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-20 20:34:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,10 +42,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word,index|
-    if index != 1
-      word << "s"
-    else word
-      end
-    end
+  array.each_with_index.collect do |word,index|{if index != 1 word << "s" else word}
   end
