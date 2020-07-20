eef091e20f05f50a0c3e9e4fb03412c9a683b85a
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-20 19:26:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,12 +16,6 @@ end
 
 def swap_elements(array)
   array.sort do |a,b|
-    if a == array[1] && b == array[2]
-      puts a
-      puts b
-      b <=> a
-      puts a
-      puts b
-  end
+    array[1], array[2] = array[2], array[1]
 end
 end
